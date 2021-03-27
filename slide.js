b648ed9916673de87d8b8abedbbd205b7fafed35
milad <miladxgh@outlook.com>
2021-03-27 18:44:57 +0430

    carousel section cleaner code

--- slide.js
+++ slide.js
@@ -16,7 +16,7 @@ showSlides();
 
 function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("carousel");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";
